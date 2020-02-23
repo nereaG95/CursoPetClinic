76dd0840befb6c6f55c472bb44956cec8b87d017
Nerea <negomez@ucm.es>
2020-02-23 19:56:24 +0100

    initial project add

---
+++ src/main/java/curso/springframework/cursopetclinic/CursopetclinicApplication.java
@@ -0,0 +1,13 @@
+package curso.springframework.cursopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursopetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursopetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/curso/springframework/cursopetclinic/CursopetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.springframework.cursopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursopetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
